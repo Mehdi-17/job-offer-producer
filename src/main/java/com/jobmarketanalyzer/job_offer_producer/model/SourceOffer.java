fa47fa24eb8ae93301e0336024ba072b2f822be1
Mehdi-17 <mehdi.serra57@gmail.com>
2024-08-18 01:09:48 +0200

    Create SourceOffer enum

---
+++ src/main/java/com/jobmarketanalyzer/job_offer_producer/model/SourceOffer.java
@@ -0,0 +1,10 @@
+package com.jobmarketanalyzer.job_offer_producer.model;
+
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+public enum SourceOffer {
+    FRANCE_TRAVAIL,
+    INDEED
+
+}
