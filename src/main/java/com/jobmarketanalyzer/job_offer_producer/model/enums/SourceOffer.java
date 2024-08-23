0d3ab0dea0ba88d0c65d9aab5523fb6e9c6e5b82
Mehdi-17 <mehdi.serra57@gmail.com>
2024-08-23 08:43:49 +0200

    clean import

--- src/main/java/com/jobmarketanalyzer/job_offer_producer/model/enums/SourceOffer.java
+++ src/main/java/com/jobmarketanalyzer/job_offer_producer/model/enums/SourceOffer.java
@@ -1,8 +1,5 @@
 package com.jobmarketanalyzer.job_offer_producer.model.enums;
 
-import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
-
 public enum SourceOffer {
     FRANCE_TRAVAIL,
     INDEED
