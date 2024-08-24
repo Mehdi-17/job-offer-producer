0d484cf8b44209a23d3c3b148be6696561bbe7af
Mehdi-17 <mehdi.serra57@gmail.com>
2024-08-24 09:04:37 +0200

    update unused import

--- src/main/java/com/jobmarketanalyzer/job_offer_producer/service/impl/IndeedService.java
+++ src/main/java/com/jobmarketanalyzer/job_offer_producer/service/impl/IndeedService.java
@@ -12,15 +12,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
-import java.time.Duration;
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
