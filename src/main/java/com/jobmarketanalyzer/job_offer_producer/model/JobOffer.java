2f7c6039af71b27be79499cd7cb85a4e4d9812d1
Mehdi-17 <mehdi.serra57@gmail.com>
2024-08-24 09:04:12 +0200

    Scrape more Data from indeed

---
+++ src/main/java/com/jobmarketanalyzer/job_offer_producer/model/JobOffer.java
@@ -0,0 +1,7 @@
+package com.jobmarketanalyzer.job_offer_producer.model;
+
+import lombok.Builder;
+
+@Builder
+public record JobOffer(String title, String description, String dailyRate) {
+}
--- src/main/java/com/jobmarketanalyzer/job_offer_producer/service/impl/IndeedService.java
+++ src/main/java/com/jobmarketanalyzer/job_offer_producer/service/impl/IndeedService.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.jobmarketanalyzer.job_offer_producer.model.JobOffer;
 import com.jobmarketanalyzer.job_offer_producer.model.JobOffersDTO;
 import com.jobmarketanalyzer.job_offer_producer.model.enums.SourceOffer;
 import com.jobmarketanalyzer.job_offer_producer.service.FetchService;
@@ -12,11 +13,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
 @Service
@@ -30,6 +37,15 @@ public class IndeedService implements FetchService {
     @Value("${indeed.job.elements}")
     private String indeedJobElementsName;
 
+    @Value("${indeed.job.title.element}")
+    private String indeedJobTitleElement;
+
+    @Value("${indeed.job.desc.element}")
+    private String indeedJobDescElement;
+
+    @Value("${indeed.job.salary.element}")
+    private String indeedJobSalaryElement;
+
     private final WebDriver driver;
     private final ObjectMapper objectMapper;
 
@@ -37,19 +53,20 @@ public class IndeedService implements FetchService {
     @Async
     public CompletableFuture<JobOffersDTO> fetchData() {
 
-            List<String> jobsText = scrapeJobOffer();
+        Set<JobOffer> jobOffers = scrapeJobOffer();
 
-            if (jobsText.isEmpty()){
-                return CompletableFuture.completedFuture(null);
-            }
+        if (jobOffers.isEmpty()) {
+            return CompletableFuture.completedFuture(null);
+        }
 
-            ArrayNode arrayNode = objectMapper.createArrayNode();
-            for (String jobText : jobsText) {
-                ObjectNode jobNode = objectMapper.createObjectNode();
-
-                jobNode.put("job", jobText);
-                arrayNode.add(jobNode);
-            }
+        ArrayNode arrayNode = objectMapper.createArrayNode();
+        for (JobOffer jobOffer : jobOffers) {
+            ObjectNode jobNode = objectMapper.createObjectNode();
+            jobNode.put("title", jobOffer.title());
+            jobNode.put("description", jobOffer.description());
+            jobNode.put("dailyRate", jobOffer.dailyRate());
+            arrayNode.add(jobNode);
+        }
 
         try {
             String json = objectMapper.writeValueAsString(arrayNode);
@@ -61,18 +78,31 @@ public class IndeedService implements FetchService {
         }
     }
 
-    private List<String> scrapeJobOffer() {
+    private Set<JobOffer> scrapeJobOffer() {
         try {
             log.info("Start headless browser to scrape indeed job offers");
             driver.get(indeedSearchUrl);
 
-            List<WebElement> elements = driver.findElements(By.cssSelector(indeedJobElementsName));
-            log.info("Scrape {} offers on indeed", elements.size());
+            List<String> jobIdList = driver.findElements(By.cssSelector(indeedJobElementsName)).stream().map(el->el.findElement(By.tagName("a")).getAttribute("id").split("_")[1]).toList();
+            log.info("Scrape {} offers on indeed", jobIdList.size());
 
-            //Todo voir pour récupérer plus de data
+            Set<JobOffer> jobOfferSet = new HashSet<>();
+
+            for (String jobId : jobIdList) {
+                String newUrl = driver.getCurrentUrl().replaceAll("(vjk=)[^&]*", "$1" + jobId);
+                driver.get(newUrl);
+
+                JobOffer scrappedOffer = JobOffer.builder()
+                        .title(driver.findElement(By.cssSelector(indeedJobTitleElement)).getText().split("-")[0].trim())
+                        .description(driver.findElement(By.id(indeedJobDescElement)).getAttribute("innerHTML"))
+                        .dailyRate(driver.findElement(By.id(indeedJobSalaryElement)).getText())
+                        .build();
+
+                jobOfferSet.add(scrappedOffer);
+            }
 
             //TODO: prendre en compte quand on a plusieurs pages de résultats.
-            return elements.stream().map(WebElement::getText).toList();
+            return jobOfferSet;
         } finally {
             log.info("Quit headless browser");
             driver.quit();
