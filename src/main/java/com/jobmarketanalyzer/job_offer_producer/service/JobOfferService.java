034fb9e761f9db4f00b02e6ad3a49651c889b523
Mehdi-17 <mehdi.serra57@gmail.com>
2024-08-30 08:33:35 +0200

    call freeworkservice in the scheduled service

--- src/main/java/com/jobmarketanalyzer/job_offer_producer/service/JobOfferService.java
+++ src/main/java/com/jobmarketanalyzer/job_offer_producer/service/JobOfferService.java
@@ -15,17 +15,20 @@ public class JobOfferService {
     private final KafkaProducerService kafkaProducerService;
     private final FetchService franceTravailService;
     private final FetchService indeedService;
+    private final FetchService freeworkService;
 
     //@Scheduled(cron = "${cron.expression}", zone = "Europe/Paris")
     @Scheduled(fixedRate = 50000000) //For development
     public void fetchOffer(){
         CompletableFuture<JobOffersDTO> franceTravailOffersFuture = franceTravailService.fetchData();
         CompletableFuture<JobOffersDTO> indeedOffersFuture = indeedService.fetchData();
-        //TODO: add freework scrapping
+        CompletableFuture<JobOffersDTO> freeworkOffersFuture = freeworkService.fetchData();
 
         //todo voir comment on vérifie que les futurs sont bon
         franceTravailOffersFuture.thenAccept(kafkaProducerService::sendJobOffer);
 
         indeedOffersFuture.thenAccept(kafkaProducerService::sendJobOffer);
+
+        freeworkOffersFuture.thenAccept(kafkaProducerService::sendJobOffer);
     }
 }
