eddf7f92cc63b045916ee6fe13d167fc5d9e664e
Mehdi-17 <mehdi.serra57@gmail.com>
2024-09-03 08:03:07 +0200

    continue to implement freework scraping

--- src/main/java/com/jobmarketanalyzer/job_offer_producer/service/impl/FreeworkService.java
+++ src/main/java/com/jobmarketanalyzer/job_offer_producer/service/impl/FreeworkService.java
@@ -3,16 +3,22 @@ package com.jobmarketanalyzer.job_offer_producer.service.impl;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jobmarketanalyzer.job_offer_producer.model.JobOffer;
-import com.jobmarketanalyzer.job_offer_producer.model.JobOffersDTO;
+import com.jobmarketanalyzer.job_offer_producer.DTO.JobOffersDTO;
 import com.jobmarketanalyzer.job_offer_producer.model.enums.SourceOffer;
 import com.jobmarketanalyzer.job_offer_producer.service.FetchService;
 import com.jobmarketanalyzer.job_offer_producer.service.JobScraper;
 import com.jobmarketanalyzer.job_offer_producer.utils.JsonUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
@@ -21,6 +27,10 @@ import java.util.concurrent.CompletableFuture;
 @Slf4j
 public class FreeworkService implements FetchService, JobScraper {
 
+    @Value("${freework.search.url}")
+    private String freeworkSearchUrl;
+
+    private final WebDriver driver;
     private final ObjectMapper objectMapper;
 
     @Override
@@ -43,6 +53,23 @@ public class FreeworkService implements FetchService, JobScraper {
     @Override
     public Set<JobOffer> scrapeJobOffer() {
         //TODO to implement
+        Set<JobOffer> jobOfferSet = new HashSet<>();
+
+        try {
+            log.info("Start headless browser to scrape freework job offers");
+            driver.get(freeworkSearchUrl);
+            //todo implementation multipage scrape.
+
+            List<WebElement> elements = driver.findElements(By.xpath("//div[@data-v-c00a56b2='']"));
+
+        }catch (Exception e){
+            log.error("There is an unexpected error when scraping Freework.", e);
+            return jobOfferSet;
+        }finally {
+            log.info("Quit freework headless browser");
+            driver.quit();
+        }
+        //todo: to remove when implementation is ok
         return null;
     }
 }
