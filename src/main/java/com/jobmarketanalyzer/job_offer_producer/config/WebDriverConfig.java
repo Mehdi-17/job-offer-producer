2c295ec1908af8fc2b7e94a2829e2a1705141ccf
Mehdi-17 <mehdi.serra57@gmail.com>
2024-09-04 08:34:59 +0200

    Create WebDriverManager to manage web driver with virtual threads

--- src/main/java/com/jobmarketanalyzer/job_offer_producer/config/WebDriverConfig.java
+++ src/main/java/com/jobmarketanalyzer/job_offer_producer/config/WebDriverConfig.java
@@ -6,15 +6,30 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @Configuration
 public class WebDriverConfig {
 
     @Bean
-    public WebDriver chromeWebDriver(){
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36");
-        options.addArguments("--headless");
+    public ExecutorService virtualThreadExecutor() {
+        return Executors.newVirtualThreadPerTaskExecutor();
+    }
 
-        return new ChromeDriver(options);
+    @Bean
+    public WebDriverManager webDriverManager() {
+        return new WebDriverManager();
+    }
+
+    public static class WebDriverManager {
+        public WebDriver createWebDriver() {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36");
+            options.addArguments("--headless");
+
+            return new ChromeDriver(options);
+        }
+
     }
 }
