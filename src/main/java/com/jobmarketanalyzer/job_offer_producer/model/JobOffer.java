8d9421df02de1bd36be483b80023d97fcc81a62b
Mehdi-17 <mehdi.serra57@gmail.com>
2024-09-17 08:10:11 +0200

    Add company to JobOffer

--- src/main/java/com/jobmarketanalyzer/job_offer_producer/model/JobOffer.java
+++ src/main/java/com/jobmarketanalyzer/job_offer_producer/model/JobOffer.java
@@ -2,8 +2,6 @@ package com.jobmarketanalyzer.job_offer_producer.model;
 
 import lombok.Builder;
 
-//todo rajouter ici et dans le consumer si une offre n'a pas de date associée, on mettra la date du scraping
-//     rajouter la compagnie qui chercher
 @Builder
-public record JobOffer(String title, String description, String dailyRate) {
+public record JobOffer(String title, String description, String dailyRate, String company) {
 }
